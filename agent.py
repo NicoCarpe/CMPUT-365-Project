7f8dcb77977be6be29e4798d294a31377745b507
HolaClubSandwich <45380530+HolaClubSandwich@users.noreply.github.com>
2019-11-30 14:24:27 -0700

    Update agent.py

--- agent.py
+++ agent.py
@@ -2,33 +2,50 @@ import numpy as np
 from tiles3 import tiles, IHT
 from agent import BaseAgent
 
-def get_fetures(maxSize, numTilings, position, velocity):
-    iht = IHT(maxSize)
-    return tiles(iht, numTilings, [velocity, position])
 
 class semi_gradient_ExpectedSarsa(BaseAgent):
     def __init__(self):
-        self.num_states = None
-        self.num_groups = None
+        self.min_position = None
+        self.max_position = None
+        self.min_velocity = None
+        self.max_velocity = None
+        self.numTilings = None
         self.step_size = None
         self.discount_factor = None
+        self.memorySize = None
+        self.epsilon = None
 
     def agent_init(self, agent_info={}):
         # set random seed for each run
         self.rand_generator = np.random.RandomState(agent_info.get("seed"))
 
         # set class attributes
-        self.num_states = agent_info.get("num_states")
+        # self.num_states = agent_info.get("num_states")
         # self.num_groups = agent_info.get("num_groups")
+        self.min_position = agent_info("min_position")
+        self.max_position = agent_info("max_position")
+        self.min_velocity = agent_info("min_velocity")
+        self.max_velocity = agent_info("max_velocity")
+
         self.step_size = agent_info.get("step_size")
         self.discount_factor = agent_info.get("discount_factor")
         self.memorySize = agent_info.get("memorySize")
         self.numTilings = agent_info.get("numTilings")
-        self.maxSize = agent_info.get("maxsize")
+        self.epsilon = agent_info.get("epsilon")
 
-        self.tile_step_size = self.step_size / self.numTilings
+        self.size_of_tiling = self.numTilings * self.numTilings
+        self.alpha = 0.1 / self.numTilings
 
         # self.all_state_fetures = np.array([])
 
         # initialize weights
-        self.weights = np.zeros(self.maxSize)
+        self.weights = np.zeros(self.size_of_tiling)
+        for i in range(len(self.weights)):
+            self.weights[i] = np.random.uniform(-0.001, 0)
+
+    def get_fetures(self, position, velocity):
+        position_scale = self.numTilings / (self.max_position - self.min_position)
+        velocity_scale = self.numTilings / (self.max_velocity - self.min_velocity)
+        iht = IHT(self.memorySize)
+        return tiles(iht, self.numTilings, [velocity_scale * velocity, position_scale * position_scale])
+        
