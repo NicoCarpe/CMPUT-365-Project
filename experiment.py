80f74a77486ea9721a1b86e520632cde8ab4c23b
HolaClubSandwich <45380530+HolaClubSandwich@users.noreply.github.com>
2019-12-03 22:54:41 -0700

    Update experiment.py

--- experiment.py
+++ experiment.py
@@ -1,110 +1,140 @@
-import agent as mc
+import agent as ag
+import numpy as np
+import environment as enviro
 import matplotlib.pyplot as plt
-from rl_glue import RLGlue
-
-# Here we provide you with the true state value and state distribution
-true_state_val = np.load('data/true_V.npy')
-state_distribution = np.load('data/state_distribution.npy')
-
-def calc_RMSVE(learned_state_val):
-    assert(len(true_state_val) == len(learned_state_val) == len(state_distribution))
-    MSVE = np.sum(np.multiply(state_distribution, np.square(true_state_val - learned_state_val)))
-    RMSVE = np.sqrt(MSVE)
-    return RMSVE
+import rlglue
 
 
-<<<<<<< HEAD
-# Define function to run experiment
-def run_experiment(environment, agent, environment_parameters, agent_parameters, experiment_parameters):
+def run_experiment(num_runs, num_episodes, agent_info, env_info):
+    all_steps = []
+    agent = ag.ExpectedSarsaAgent()
+    env = enviro.MountainEnvironment()
 
-    rl_glue = RLGlue(environment, agent)
+    for run in range(num_runs):
+        if run % 5 == 0:
+            print("RUN: {}".format(run))
 
-    # Sweep Agent parameters
-    for num_agg_states in agent_parameters["num_groups"]:
-        for step_size in agent_parameters["step_size"]:
+        rl_glue = rlglue.RLGlue(env, agent)
+        rl_glue.rl_init(agent_info, env_info)
+        steps_per_episode = []
 
-            # save rmsve at the end of each evaluation episode
-            # size: num_episode / episode_eval_frequency + 1 (includes evaluation at the beginning of training)
-            agent_rmsve = np.zeros(int(experiment_parameters["num_episodes"]/experiment_parameters["episode_eval_frequency"]) + 1)
+        for episode in range(num_episodes):
+            # no sure why 15000
+            rl_glue.rl_episode(15000)
+            steps_per_episode.append(rl_glue.num_steps)
+        
+        all_steps.append(np.array(steps_per_episode))
+    
+    plt.plot(np.mean(np.array(all_steps), axis = 0))
+    np.save("ExpectedSarsa_test", np.array(all_steps))
 
-            # save learned state value at the end of each run
-            agent_state_val = np.zeros(environment_parameters["num_states"])
+def main():
+    num_runs = 50
+    num_episodes = 200
+    initial_weights = np.zeros((3, 4096))
+    for action in range(3):
+        for i in range(4096):
+            initial_weights[action][i] = np.random.uniform(-0.001, 0)
+    start_position = np.random.uniform(-0.6, -0.4)
+    agent_info = {"num_tilings": 8, "num_tiles": 8, "iht_size": 4096, "epsilon": 0.0, "gamma": 1, "alpha": 0.1/8, "initial_weights": initial_weights, "num_actions": 3}
+    env_info = {"min_position": -1.2, "max_position": 0.5, "min_velocity": -0.07, "max_velocity": 0.07, "gravity": 0.0025, "start_position": start_position, "start_velocity": 0.0, "action_discount": 0.001}
 
-            env_info = {"num_states": environment_parameters["num_states"],
-                        "start_state": environment_parameters["start_state"],
-                        "left_terminal_state": environment_parameters["left_terminal_state"],
-                        "right_terminal_state": environment_parameters["right_terminal_state"]}
-
-            agent_info = {"num_states": environment_parameters["num_states"],
-                          "num_groups": num_agg_states,
-                          "step_size": step_size,
-                          "discount_factor": environment_parameters["discount_factor"]}
-
-            print('Setting - num. agg. states: {}, step_size: {}'.format(num_agg_states, step_size))
-            os.system('sleep 0.2')
-
-            # one agent setting
-            for run in tqdm(range(1, experiment_parameters["num_runs"]+1)):
-                env_info["seed"] = run
-                agent_info["seed"] = run
-                rl_glue.rl_init(agent_info, env_info)
-
-                # Compute initial RMSVE before training
-                current_V = rl_glue.rl_agent_message("get state value")
-                agent_rmsve[0] += calc_RMSVE(current_V)
-
-                for episode in range(1, experiment_parameters["num_episodes"]+1):
-                    # run episode
-                    rl_glue.rl_episode(0) # no step limit
-
-                    if episode % experiment_parameters["episode_eval_frequency"] == 0:
-                        current_V = rl_glue.rl_agent_message("get state value")
-                        agent_rmsve[int(episode/experiment_parameters["episode_eval_frequency"])] += calc_RMSVE(current_V)
-
-                # store only one run of state value
-                if run == 50:
-                    agent_state_val = rl_glue.rl_agent_message("get state value")
-
-            # rmsve averaged over runs
-            agent_rmsve /= experiment_parameters["num_runs"]
-
-            save_name = "{}_agg_states_{}_step_size_{}".format('TD_agent', num_agg_states, step_size).replace('.','')
-
-            if not os.path.exists('results'):
-                os.makedirs('results')
-
-            # save avg. state value
-            np.save("results/V_{}".format(save_name), agent_state_val)
-
-            # save avg. rmsve
-            np.save("results/RMSVE_{}".format(save_name), agent_rmsve)
-=======
-def test_TileCoder():
-    tests = [[-1.0, 0.01], [0.1, -0.01], [0.2, -0.05], [-1.0, 0.011], [0.2, -0.05]]
-    mctc = mc.MountainCarTileCoder(iht_size=1024, num_tilings=8, num_tiles=8)
-    t = []
-    for test in tests:
-        position, velocity = test
-        tiles = mctc.get_tiles(position=position, velocity=velocity)
-        t.append(tiles)
-
-    print("Your results:")
-    for tiles in t:
-        print(tiles)
-
-    print()
-    print("Expected results:")
-    expected = """[0 1 2 3 4 5 6 7]
-    [ 8  9 10 11 12 13 14 15]
-    [16 17 18 19 20 21 22 23]
-    [ 0 24  2  3  4  5  6  7]
-    [16 17 18 19 20 21 22 23]
-    """
-    print(expected)
+    run_experiment(num_runs, num_episodes, agent_info, env_info)
+main()
 
 
-def run_experiment(environment, agent, environment_parameters, agent_parameters, experiment_parameters):
-    rl_glue = RLGlue(environment, agent)
 
-    for 
->>>>>>> 41f8b3ffcc645c550d2e0a03352c0b048464b3f9
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Here we provide you with the true state value and state distribution
+# true_state_val = np.load('data/true_V.npy')
+# state_distribution = np.load('data/state_distribution.npy')
+
+# def calc_RMSVE(learned_state_val):
+#     assert(len(true_state_val) == len(learned_state_val) == len(state_distribution))
+#     MSVE = np.sum(np.multiply(state_distribution, np.square(true_state_val - learned_state_val)))
+#     RMSVE = np.sqrt(MSVE)
+#     return RMSVE
+
+
+# # Define function to run experiment
+# def run_experiment(environment, agent, environment_parameters, agent_parameters, experiment_parameters):
+
+#     rl_glue = RLGlue(environment, agent)
+
+#     # Sweep Agent parameters
+#     for num_agg_states in agent_parameters["num_groups"]:
+#         for step_size in agent_parameters["step_size"]:
+
+#             # save rmsve at the end of each evaluation episode
+#             # size: num_episode / episode_eval_frequency + 1 (includes evaluation at the beginning of training)
+#             agent_rmsve = np.zeros(int(experiment_parameters["num_episodes"]/experiment_parameters["episode_eval_frequency"]) + 1)
+
+#             # save learned state value at the end of each run
+#             agent_state_val = np.zeros(environment_parameters["num_states"])
+
+#             env_info = {"num_states": environment_parameters["num_states"],
+#                         "start_state": environment_parameters["start_state"],
+#                         "left_terminal_state": environment_parameters["left_terminal_state"],
+#                         "right_terminal_state": environment_parameters["right_terminal_state"]}
+
+#             agent_info = {"num_states": environment_parameters["num_states"],
+#                           "num_groups": num_agg_states,
+#                           "step_size": step_size,
+#                           "discount_factor": environment_parameters["discount_factor"]}
+
+#             print('Setting - num. agg. states: {}, step_size: {}'.format(num_agg_states, step_size))
+#             os.system('sleep 0.2')
+
+#             # one agent setting
+#             for run in tqdm(range(1, experiment_parameters["num_runs"]+1)):
+#                 env_info["seed"] = run
+#                 agent_info["seed"] = run
+#                 rl_glue.rl_init(agent_info, env_info)
+
+#                 # Compute initial RMSVE before training
+#                 current_V = rl_glue.rl_agent_message("get state value")
+#                 agent_rmsve[0] += calc_RMSVE(current_V)
+
+#                 for episode in range(1, experiment_parameters["num_episodes"]+1):
+#                     # run episode
+#                     rl_glue.rl_episode(0) # no step limit
+
+#                     if episode % experiment_parameters["episode_eval_frequency"] == 0:
+#                         current_V = rl_glue.rl_agent_message("get state value")
+#                         agent_rmsve[int(episode/experiment_parameters["episode_eval_frequency"])] += calc_RMSVE(current_V)
+
+#                 # store only one run of state value
+#                 if run == 50:
+#                     agent_state_val = rl_glue.rl_agent_message("get state value")
+
+#             # rmsve averaged over runs
+#             agent_rmsve /= experiment_parameters["num_runs"]
+
+#             save_name = "{}_agg_states_{}_step_size_{}".format('TD_agent', num_agg_states, step_size).replace('.','')
+
+#             if not os.path.exists('results'):
+#                 os.makedirs('results')
+
+#             # save avg. state value
+#             np.save("results/V_{}".format(save_name), agent_state_val)
+
+#             # save avg. rmsve
+#             np.save("results/RMSVE_{}".format(save_name), agent_rmsve)
